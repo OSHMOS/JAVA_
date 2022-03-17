edabbb58011aadea6d6097580db67209d49f92e6
OSHMOS <ojames9707@gmail.com>
2022-03-17 11:32:54 +0900

    Java : private 다루기 (접근 제어자 다루기)

--- codeit/src/BankSimulation/BankAccount.java
+++ codeit/src/BankSimulation/BankAccount.java
@@ -1,23 +1,36 @@
 package BankSimulation;
 
 public class BankAccount {
-    int balance;
-    Person owner;
+    private int balance;
+    private Person owner;
 
-    // 파라미터 : 입금할 액수 (정수)
+    public void setBalance (int newBalance) {
+        balance = newBalance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setOwner (Person newOwner) {
+        owner = newOwner;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    // 파라미터 : 입금할 액수 (정수) 
     // 리턴 : 성공 여부 (불린)
 
     boolean deposit(int amount) {
-        if(amount < 0){
-            System.out.println(String.format("입금 실패입니다. 잔고: %d원, 현금: %d원", owner.account.balance, owner.cashAmount));
-            return false;
-        }else if(owner.cashAmount < amount){
-            System.out.println(String.format("입금 실패입니다. 잔고: %d원, 현금: %d원", owner.account.balance, owner.cashAmount));
+        if(amount < 0 || owner.getCashAmount() < amount){
+            System.out.println(String.format("입금 실패입니다. 잔고: %d원, 현금: %d원", balance, owner.getCashAmount()));
             return false;
         }else {
-            owner.account.balance += amount;
-            owner.cashAmount -= amount;
-            System.out.println(String.format("%d원 입금하였습니다. 잔고: %d원, 현금: %d원", amount, owner.account.balance, owner.cashAmount));
+            owner.setCashAmount(owner.getCashAmount() - amount);
+            balance += amount;
+            System.out.println(String.format("%d원 입금하였습니다. 잔고: %d원, 현금: %d원", amount, balance, owner.getCashAmount()));
             return true;
         }
     }
@@ -26,16 +39,13 @@ public class BankAccount {
     // 리턴 : 성공 여부 (불린)
 
     boolean withdraw(int amount) {
-        if(amount < 0){
-            System.out.println(String.format("입금 실패입니다. 잔고: %d원, 현금: %d원", owner.account.balance, owner.cashAmount));
-            return false;
-        }else if(owner.account.balance < amount){
-            System.out.println(String.format("입금 실패입니다. 잔고: %d원, 현금: %d원", owner.account.balance, owner.cashAmount));
+        if(amount < 0 || amount > balance){
+            System.out.println(String.format("출금 실패입니다. 잔고: %d원, 현금: %d원", balance, owner.getCashAmount()));
             return false;
         }else {
-            owner.account.balance -= amount;
-            owner.cashAmount += amount;
-            System.out.println(String.format("%d원 입금하였습니다. 잔고: %d원, 현금: %d원", amount, owner.account.balance, owner.cashAmount));
+            owner.setCashAmount(owner.getCashAmount() + amount);
+            balance -= amount;
+            System.out.println(String.format("%d원 출금하였습니다. 잔고: %d원, 현금: %d원", amount, balance, owner.getCashAmount()));
             return true;
         }
     }
